ce8d1e3855da3e2f99a5a2630dd22940ee354ff3
Kenneth <Kcolisah084@gmail.com>
2026-07-08 10:49:50 -0500

    Hide private public files (#10)
    * feat: finalize mobile experience
    
    * style: refine desktop and mobile compare pages
    
    * asset: add compare hero motorcycle image
    
    * style: finalize shared page cleanup
    
    * chore: remove private files from public tracking

--- motorcycle-admin/JavaScript/adminConfig.js
+++
@@ -1,8 +0,0 @@
-window.MT_ADMIN_CONFIG = {
-  loginEndpoint: "/api/admin/login",
-  updatesEndpoint: "/api/admin/updates",
-  tokenStorageKey: "mtAdminToken",
-  sessionStorageKey: "mtAdminMode",
-  localUpdatesStorageKey: "mtCustomUpdates",
-  requireLogin: false
-};
